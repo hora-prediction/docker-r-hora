37d8e059ad0368f49823c0514fc780b463871269
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2017-03-19 00:24:15 +0100

    Add pROC library

--- install-libraries.R
+++ install-libraries.R
@@ -1,3 +1,4 @@
 install.packages('Rserve', repos='https://cran.r-project.org')
 install.packages('bnlearn', repos='https://cran.r-project.org')
 install.packages('forecast', repos='https://cran.r-project.org')
+install.packages('pROC', repos='https://cran.r-project.org')
--- start.R
+++ start.R
@@ -3,5 +3,6 @@ library(Rserve)
 #load libraries so all connections will share them
 library("bnlearn")
 library("forecast")
+library("pROC")
 
 run.Rserve(port = 6311, remote=TRUE, auth=FALSE, config.file = "/etc/Rserve.conf")
